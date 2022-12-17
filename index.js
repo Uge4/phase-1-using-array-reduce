b7d82f45381e8f09555c380e5fe1407a562d9ce4
eugene <eugeneholenstein@gmail.com>
2022-12-17 15:32:16 +1100

    completed using array reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// reduce iterates over array
+// accumulator variable or callback function, starting element in array by index
+const totalBatteries = batteryBatches.reduce(function(batchAggregator, batch) {return batch + batchAggregator}, 0)
+
+
+console.log(totalBatteries);
